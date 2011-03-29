dfc659449daa334820e4526de3efaf964b773417
edgar.guevara <edgar.guevara@9ba24e85-b08d-2ac1-2900-37632f61724d>
2011-03-29 21:16:24 +0000

    Ethernet test OK!

---
+++ DE4_SSS/DE4_Ethernet_0/sopc_add_qip_file.tcl
@@ -0,0 +1,3 @@
+project_open -current_revision "D:/Work/Project/Proj_DE4/System_Builder/CodeGenerated/DE4/DE4_230_Ethernet_0/DE4_230_Ethernet.qpf"
+set_global_assignment -name QIP_FILE "DE4_SOPC.qip"
+project_close
