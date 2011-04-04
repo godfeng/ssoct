58a4f3d62727cbc07d0b7b7fd51528ed469b8fa9
edgar.guevara <edgar.guevara@9ba24e85-b08d-2ac1-2900-37632f61724d>
2011-04-04 22:01:18 +0000

    Removed locked folder, unable to perform cleanup. .metadata not a working copy directory

--- DE4_SSS/DE4_Ethernet_0/sopc_add_qip_file.tcl
+++
@@ -1,3 +0,0 @@
-project_open -current_revision "D:/Work/Project/Proj_DE4/System_Builder/CodeGenerated/DE4/DE4_230_Ethernet_0/DE4_230_Ethernet.qpf"
-set_global_assignment -name QIP_FILE "DE4_SOPC.qip"
-project_close
