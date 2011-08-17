9fe07b2ae210b3b35143ec9401f3963454494870
edgar.guevara <edgar.guevara@9ba24e85-b08d-2ac1-2900-37632f61724d>
2011-08-17 23:09:10 +0000

    Turtoise cleanup: deleted DE4_230_DSP_HSMB, DE4_SSS, fft_32K, ifft_fft_unitygain91, PWM_test

--- FPGA/DE4_SSS/RAM_inst.v
+++
@@ -1,8 +0,0 @@
-RAM	RAM_inst (
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.rdaddress ( rdaddress_sig ),
-	.wraddress ( wraddress_sig ),
-	.wren ( wren_sig ),
-	.q ( q_sig )
-	);
--- FPGA/PWM_test/clock_divider_inst.v
+++
@@ -1,4 +0,0 @@
-clock_divider	clock_divider_inst (
-	.clock ( clock_sig ),
-	.q ( q_sig )
-	);
