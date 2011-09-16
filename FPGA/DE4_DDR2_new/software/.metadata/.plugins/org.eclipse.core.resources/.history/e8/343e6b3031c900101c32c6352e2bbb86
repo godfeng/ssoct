721052d94a47c28052e59f7f870a8ce0e55e9110
edgar.guevara <edgar.guevara@9ba24e85-b08d-2ac1-2900-37632f61724d>
2011-09-16 14:12:28 +0000

    software folder deleted

--- FPGA/DE4_DDR2_new/software/.metadata/.plugins/org.eclipse.core.resources/.history/e8/343e6b3031c900101c32c6352e2bbb86
+++
@@ -1,2 +0,0 @@
-obj/alt_mcount.o:  \
- /cygdrive/c/altera/91/ip/altera/nios2_ip/altera_nios2/HAL/src/alt_mcount.S
--- FPGA/DE4_DDR2_new/software/DE4_DDR2/mem_test.h
+++
@@ -1,7 +0,0 @@
-#ifndef MEM_TEST_H_
-#define MEM_TEST_H_
-
-bool TMEM_Verify(alt_u32 BaseAddr, alt_u32 ByteLen, alt_u32 InitValue);
-
-
-#endif /*MEM_TEST_H_*/
