be8d360eeff5e8b4d7bb623fbc7fede73f1702ab
edgar.guevara <edgar.guevara@9ba24e85-b08d-2ac1-2900-37632f61724d>
2012-03-09 20:55:40 +0000

    sweptSourceOCT.llb displays schematics live. reconstruction_steps.m displays raw, theoretical and interpolated data

--- Matlab/Misc/reconstruction_steps.m
+++ Matlab/Misc/reconstruction_steps.m
@@ -52,8 +52,9 @@ tmpCorrArray = rectwin(NSAMPLES);
 Aline_corr = Aline_corr.*tmpCorrArray;
 
 % Resampling (Interpolation/Decimation) along columns (A-lines)
-Aline_corr = interp1(k0, Aline_corr, fixedSampling, 'linear');
-figure(1); plot(Aline_corr,'r-')
+Aline_interp = interp1(k0, Aline_corr, fixedSampling, 'linear');
+figure(1); plot(Aline_interp,'r-')
+hold on; plot(Aline_corr,'b-')
 
 % Theoretical sinus
 fs = 125e6;
@@ -61,7 +62,7 @@ ts = 1/fs;
 t = (0:NSAMPLES-1)*ts; 
 A_theo = 5*sin(2*pi*1.1e6*t - 0.3)';
 figure (1); hold on; plot(A_theo, 'k-')
-legend({'Experimental' 'Theoretical'})
+legend({'Exp. Interpolated' 'Exp. Raw' 'Theoretical'})
 
 %%
 % fourier transform on 4096 points
@@ -71,21 +72,24 @@ ssOCTdefaults.nSamplesFFT = NFFT;
 ssOCTdefaults.range.posZaxis_air        = linspace(ssOCTdefaults.range.delta_Z_Nq_air ./...
     ssOCTdefaults.nSamplesFFT,ssOCTdefaults.range.delta_Z_Nq_air,ssOCTdefaults.nSamplesFFT/2);
 z = ssOCTdefaults.range.posZaxis_air;
-Afft = abs(fftshift(fft(Aline_corr,NFFT,1),1));
+A_corr_fft = abs(fftshift(fft(Aline_corr,NFFT,1),1));
+Afft = abs(fftshift(fft(Aline_interp,NFFT,1),1));
 Rfft = abs(fftshift(fft(ref,NFFT,1),1));
-%figure(2); plot(Afft,'k-')
 
 % Keep only positive half
 Afft_half = Afft(NFFT/2+1:end);
-% figure(3); plot(Afft_half,'k-')
-
+Afft_corr_half = A_corr_fft(NFFT/2+1:end);
 
 % Hi-pass
 Afft_filter = Afft_half;
 Afft_filter(1:5) = 0;
+Afft_corr_filter = Afft_corr_half;
+Afft_corr_filter(1:5) = 0;
 % Normalize by the maximum
 Afft_filter = Afft_filter ./ max(Afft_filter);
+Afft_corr_filter = Afft_corr_filter ./ max(Afft_corr_filter);
 figure(3); hold on; plot(z, Afft_filter, 'r-')
+plot(z, Afft_corr_filter, 'b:')
 
 % FWHM
 [FWHM, peak_pos, FWHM_um, peak_pos_m] = fwhm(Afft_filter);
@@ -105,5 +109,5 @@ fprintf('Theoretical FWHM: %.4f um\n',FWHM_um)
 fprintf('Axial resolution: %.4f um\n',1e6*ssOCTdefaults.axial.zr_air)
 figure(3); hold on; plot(peak_pos_m, A_theo_fft(peak_pos), 'ko')
 xlim ([0 500e-6])
-legend({'Experimental' 'Exp FWHM' 'Theoretical' 'Theo FWHM'})
+legend({'Exp. Interpolated' 'Exp. Raw' 'Exp FWHM' 'Theoretical' 'Theo FWHM'})
 
